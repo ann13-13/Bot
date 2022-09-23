a3d687209b0b5c3661eb15435ee76803647412ee
Alekseeva <kikololwhat@gmail.com>
2022-09-23 15:49:27 +0300

    EchoBot

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+from aiogram import Bot, Dispatcher, executor
+from config import API_TOKEN
+
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+loop = asyncio.get_event_loop()
+bot = Bot(API_TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot, loop=loop)
+
+if __name__ == '__main__':
+    from handlers import dp, send_to_admin
+    executor.start_polling(dp, on_startup=send_to_admin)
+
+API_TOKEN = '5716740580:AAF8NGJT0O893WivrHYR5j4dfLLZm5OgizU'
+
+
+
+
+
+
+
